9d5629e4d6abb230284e30a373754a6a9ec11a38
tpwonder <97324746+tpwonder@users.noreply.github.com>
2022-05-05 15:31:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
-echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> 'feeds.conf.default'
+#echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> 'feeds.conf.default'
